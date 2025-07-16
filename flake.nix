d7de978c44a8fc8715fc3aaccbd838c29daf0db5
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-07-16 20:41:13 +0200

    fix FID problem

--- flake.nix
+++ flake.nix
@@ -13,8 +13,28 @@
     overlays.default = final: prev: {
       build-all = self.packages.${prev.system}.default;
     };
-    packages = utils.lib.eachSystem { } (pkgs: {
-      default = pkgs.haskellPackages.callCabal2nix "build-all" ./. { };
-    });
+    packages = utils.lib.eachSystem { } (pkgs:
+      let
+        build-all = pkgs.haskellPackages.mkDerivation {
+          pname = "build-all";
+          version = "0.1.0.0";
+          src = ./.;
+          isLibrary = false;
+          isExecutable = true;
+          executableHaskellDepends = with pkgs.haskellPackages; [
+            aeson
+            base
+            bytestring
+            directory
+            filepath
+            optparse-applicative
+            process
+          ];
+          homepage = "https://github.com/NewDawn0/buildAll";
+          description = "A tool to build all outputs of a Nix flake";
+          license = pkgs.lib.licenses.mit;
+          mainProgram = "build-all";
+        };
+      in { default = build-all; });
   };
 }
